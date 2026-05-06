577145436aaa35d729dfcc66382e3717f1cfb65a
E-sellers <yahia@e-sellers.net>
2026-05-06 10:49:05 +0300

    Fix Express 5 wildcard route syntax (* -> /{*path})

--- server.ts
+++ server.ts
@@ -50,7 +50,7 @@ const __serverDir = path.dirname(fileURLToPath(import.meta.url));
 const clientPath = path.join(__serverDir, 'client');
 console.log(`Serving frontend from: ${clientPath}`);
 app.use(express.static(clientPath));
-app.get('*', (_req, res) => {
+app.get('/{*path}', (_req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
